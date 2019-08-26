fded1c2de30f8d9e01e957125cf4bec52331a5c6
Nur Ardia Rahmaningrum <nurardiar@gmail.com>
2019-08-26 13:32:38 +0700

    12 - jurnal lib + datepicker neraca awal

---
+++ app/src/main/java/com/ardiarahma/sik_bumdesa/activities/dashboard/JurnalDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.ardiarahma.sik_bumdesa.activities.dashboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JurnalDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jurnal_details);
+    }
+}
